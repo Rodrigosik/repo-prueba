80fb58094d32fb6278219da5ba62570e2d172360
Rodrigo Sandoval <sandovalrgg@gmail.com>
2026-02-10 19:56:00 -0600

    feat(home): Agregando pantalla de home

--- angular-patitas/src/app/app.routes.ts
+++ angular-patitas/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./ui/home/home').then((m) => m.Home),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+];
