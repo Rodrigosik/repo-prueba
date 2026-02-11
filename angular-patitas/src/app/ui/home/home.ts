98b28997a09073c1831c435a3c22b5316e78e326
Rodrigo Sandoval <sandovalrgg@gmail.com>
2026-02-10 21:20:28 -0600

    feat(home): Agrega navegación al botón Agendar
    - Añade el atributo routerLink al botón Agendar en la vista principal
    - Importa RouterModule en el componente Home para habilitar la navegación
    - Mejora la experiencia de usuario permitiendo agendar desde la página de inicio

--- angular-patitas/src/app/ui/home/home.ts
+++ angular-patitas/src/app/ui/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FreyButtonDirective } from 'freya';
 
 @Component({
   selector: 'app-home',
-  imports: [FreyButtonDirective],
+  imports: [FreyButtonDirective, RouterModule],
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
 })
