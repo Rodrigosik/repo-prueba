4db8790d5f2fc949210a1d8433952f794748046c
Rodrigo Sandoval <sandovalrgg@gmail.com>
2026-02-10 21:17:13 -0600

    feat(appointment): Agregando pantalla de agenda

--- angular-patitas/src/app/app.routes.ts
+++ angular-patitas/src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./ui/home/home').then((m) => m.Home),
+    loadComponent: () => import('./ui/home/home').then(m => m.Home),
+  },
+  {
+    path: 'agendar',
+    loadComponent: () => import('./ui/appointment/appointment').then(m => m.Appointment),
   },
   {
     path: '**',
---
+++ angular-patitas/src/app/ui/appointment/appointment.ts
@@ -0,0 +1,37 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FreyButtonDirective } from 'freya/button';
+import { FreyFormModule } from 'freya/form';
+
+@Component({
+  selector: 'app-appointment',
+  imports: [ReactiveFormsModule, FreyFormModule, FreyButtonDirective],
+  templateUrl: './appointment.html',
+  styleUrl: './appointment.scss',
+})
+export class Appointment {
+  formGroup: FormGroup;
+
+  private readonly formBuilder = inject(FormBuilder);
+
+  constructor() {
+    this.buildForm();
+  }
+
+  onSubmit(): void {
+    if (this.formGroup.invalid) {
+      this.formGroup.markAllAsTouched();
+      return;
+    }
+
+    console.log('Form submitted:', this.formGroup.value);
+  }
+  private buildForm(): void {
+    this.formGroup = this.formBuilder.group({
+      clientName: ['', [Validators.required]],
+      petName: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+    });
+  }
+}
